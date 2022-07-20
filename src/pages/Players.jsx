d884a6698fb3a5ce43d661321f91d09bd8883f39
Poovendharan Selvaraj <97455003+PoovendharanSelvaraj@users.noreply.github.com>
2022-07-20 21:00:57 +0530

    LoginPage

--- src/pages/Players.jsx
+++ src/pages/Players.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Box, Flex } from '@chakra-ui/react'
 
 const Players = () => {
   return (
-    <div>Players</div>
+    <Box   h="600px"bg={"white"} w='75%' margin ="auto" border={"2px solid red"}>
+    This is the Bo
+     <Box w="95%" border={"1px solid black"} margin="auto" >
+       <Flex h={"400px"} justifyContent="space-around">
+          <Box w={"33%"} border="1px solid blue" margin={"auto"} >     
+              
+          </Box>
+          <Box w={"66%"} border="1px solid blue" margin={"auto"}>
+              <Flex w={"100%"} h="50%" flexFlow={"row"}>
+              <Box w={"50%"}  border="1px solid blue"></Box>  
+              <Box w="25%" border="1px solid blue"></Box>
+              <Box w="25%" border="1px solid blue"></Box>  
+              </Flex>   
+          <Flex w={"100%"} h="50%">
+              <Box w="25%" border="1px solid blue"></Box>
+              <Box w="25%" border="1px solid blue"></Box>
+              <Box w="25%" border="1px solid blue"></Box>
+              <Box w="25%" border="1px solid blue"></Box>    
+          </Flex>    
+          </Box>     
+        
+        </Flex>  
+    
+
+     </Box>
+  </Box>
   )
 }
 
