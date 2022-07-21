19bb24db83869c986d5d41eebbdbe92706b290f1
Vikash Kumar <87219068+VikashKumarNokha@users.noreply.github.com>
2022-07-22 03:20:59 +0530

    players

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
    import Footer from "./components/footer/Footer";
-// import LoginPage from "./Pages/LoginPage";
 import MainRoute from "./Pages/MainRoute";
 import Players from "./Pages/Players";
 import Videos from "./Pages/Videos";
 function App() {
   return <div>
-       <Players/>   
-       <MainRoute></MainRoute>
+       {/* <Players/>   */}
+      <MainRoute></MainRoute>
       <Footer></Footer>
-      {/* <LoginPage/> */}
   </div>;
 }
 
--- src/redux/Playersreducer/reducer.js
+++ src/redux/Playersreducer/reducer.js
@@ -1,1 +1,24 @@
 // Todo
+ import  * as types from "./action.types"
+ const initialstate = {
+     players : [],
+     isLoading : true,
+     isError : false,
+ }
+
+const playersreducer = (state = initialstate, action)=>{
+        const {type, payload} = action;
+    switch(type){
+          case types.GET_PLAYERS_REQUEST :
+           return {...state, isLoading : true, isError : false };
+           case types.GET_PLAYERS_SUCCESS :
+            console.log("payload");
+            return  { ...state, isLoading : false, isError: false, players : payload };
+            case types.GET_PLAYERS_FAILURE :
+                return { ...state, isLoading : false, isError : true, };
+       default :
+        return state;  
+    } 
+}
+
+export default  playersreducer;
