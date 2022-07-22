dbc4f59f3c8549a36a19af0588b0b186a127aca6
Ritu Chauhan <93373957+architachauhan152@users.noreply.github.com>
2022-07-22 22:17:19 +0530

    Update Homepage.jsx

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
-import Slider from "./Slider"
-import FeathuredVideo from './FeathuredVideo'
 
 
-import DataDigest from './DataDigest';
+
+
 
 
 const Homepage = () => {
   return (
     <div>
-      <Slider/>
-      <FeathuredVideo/>
-      
-      <DataDigest/>
+
     </div>
   )
 }
