f84958f97a04772f1e061eaaf40b6021d1c3cebc
shashankfeb16 <101566555+shashankfeb16@users.noreply.github.com>
2022-07-23 23:40:22 +0530

    update in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import {legacy_createStore,applyMiddleware,combineReducers} from "redux";
 import thunk from "redux-thunk";
 import playersreducer from "./Playersreducer/reducer";
+import seriesUpcomingReducer from "./seriesPageReducer/reducer";
 // import { authReducer } from "./auth/authreducer";
 // import { reducer } from "./books/reducer";
 const rootReducer = combineReducers({
      players : playersreducer,
+     series: seriesUpcomingReducer
     // book: reducer,
     // auth: authReducer
 })
